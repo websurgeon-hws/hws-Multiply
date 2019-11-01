c373b7c69027d000a45ac59db4a6d4936cb2853c
Peter Barclay <peter@peter-barclay.com>
2019-11-01 17:40:26 +0000

    implement GameView navigation

--- Multiply/GameView.swift
+++ Multiply/GameView.swift
@@ -5,27 +5,54 @@
 import SwiftUI
 
 struct GameView: View {
-    private enum GameState {
+    enum GameState {
         case settings
         case running
     }
+        
+    @State var gameState: GameState = .settings
 
-    @State private var gameState = GameState.settings
-    
     var body: some View {
         initialView()
     }
     
     private func initialView() -> some View {
         switch self.gameState {
-        case .settings: return AnyView(SettingsView())
-        case .running: return AnyView(RunningView())
+        case .settings: return AnyView(settingsView())
+        case .running: return AnyView(runningView())
+        }
+    }
+    
+    private func settingsView() -> some View {
+        NavigationView {
+            SettingsView()
+                .navigationBarTitle("Settings")
+                .navigationBarItems(leading:
+                    Button("Start") {
+                        self.gameState = .running
+                    }
+                )
+        }
+    }
+    
+    private func runningView() -> some View {
+        NavigationView {
+            RunningView()
+            .navigationBarTitle("Multiply")
+            .navigationBarItems(leading:
+                Button("Settings") {
+                    self.gameState = .settings
+                }
+            )
         }
     }
 }
 
 struct GameView_Previews: PreviewProvider {
     static var previews: some View {
-        GameView()
+        Group {
+            GameView(gameState: .settings)
+            GameView(gameState: .running)
+        }
     }
 }
